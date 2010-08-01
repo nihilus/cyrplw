9f745934f7cbac8072b0272ec782a3d2d53e5fac
redplait <redplait@b0c7a04d-6c7b-4c1e-b982-2d7b310faa71>
2010-08-01 09:41:56 +0000

    plugin for MachO PPC position independent code processing

---
+++ MachOPIC/ppc_e.cpp
@@ -0,0 +1,161 @@
+#include "ppc_e.h"
+#include <allins.hpp>
+
+char *ppc_lemul::reg_name(int r)
+{
+  if ( r >= ph.regsNum )
+   return "hz";
+  return ph.regNames[r];
+}
+
+ppc_lemul::ppc_lemul(smac_reffer *ref)
+{
+  m_reffer = ref;
+  reset();
+}
+
+void ppc_lemul::reset()
+{
+  m_basereg = 0;
+  m_status = 0;
+  memset(m_regs, 0, sizeof(m_regs));
+}
+
+int ppc_lemul::is_pic_b(ea_t ea)
+{
+  if ( cmd.itype != PPC_b )
+   return 0;
+  if ( !exists_addr(&cmd.Op1) )
+   return 0;
+  if ( cmd.Op1.addr != ea + cmd.size )
+   return 0;
+  return 1;
+}
+
+int ppc_lemul::is_mflr()
+{
+  if ( cmd.itype != PPC_mflr )
+   return 0;
+  if ( cmd.Op2.type != 8 )
+   return 0;
+  if ( cmd.Op1.type != o_reg )
+   return 0;
+  m_basereg = cmd.Op1.reg;
+  m_regs[m_basereg].value = m_base;
+  m_regs[m_basereg].is_const = 1;
+  return 1;
+}
+
+void ppc_lemul::do_lis()
+{
+  int reg = cmd.Op1.reg;
+  if ( reg == m_basereg )
+  {
+    reset();
+    return;
+  }
+  unsigned long value = (cmd.Op2.value << 16) & 0xffff0000;
+  if ( m_regs[reg].is_const )
+   m_regs[reg].value = value;
+  else {
+   m_regs[reg].is_const = 1;
+   m_regs[reg].value = value;
+  }
+}
+
+void ppc_lemul::do_ori()
+{
+  int reg1 = cmd.Op1.reg;
+  if ( reg1 == m_basereg )
+  {
+    reset();
+    return;
+  }
+  int reg2 = cmd.Op2.reg;
+  if ( reg1 == reg2 )
+  {
+    if ( m_regs[reg1].is_const )
+     m_regs[reg1].value |= cmd.Op3.value;
+    return;
+  }
+  m_regs[reg1].is_const = 0;
+}
+
+void ppc_lemul::do_cmt(ea_t a, int reg, ea_t value)
+{
+  ea_t real_value = value;
+  if ( m_reffer != NULL )
+   real_value = m_reffer->add(a, value);
+  qsnprintf(m_cmt, sizeof(m_cmt), "%s %X", reg_name(reg), real_value);
+  set_cmt(a, m_cmt, false);
+}
+
+void ppc_lemul::do_add(ea_t a)
+{
+  int reg_D = cmd.Op1.reg;
+  int reg_A = cmd.Op2.reg;
+  int reg_B = cmd.Op3.reg;
+  // check what we add
+  if ( reg_A == m_basereg || reg_B == m_basereg )
+  {
+    if ( m_regs[reg_A].is_const &&
+         m_regs[reg_B].is_const )
+    {
+      m_regs[reg_D].is_const = 1;
+      m_regs[reg_D].value = m_regs[reg_A].value + m_regs[reg_B].value;
+      do_cmt(a, reg_D, m_regs[reg_D].value);
+      return;
+    } else {
+      reset();
+      return;
+    }
+  }
+  // finally check what reg changed
+  if ( reg_D == m_basereg )
+   reset();
+}
+
+int ppc_lemul::emul(ea_t a)
+{
+  if ( !m_status )
+  {
+    if ( is_pic_b(a) )
+    {
+      m_status = 1;
+      m_base = cmd.Op1.addr;
+    }
+    return m_status;
+  }
+  if ( m_status == 1 )
+  {
+    if ( is_mflr() )
+    {
+      m_status = 2;
+    } else {
+      reset();
+    }
+    return m_status;
+  }
+  // main processing - after b $+4, mflr
+  if ( m_status == 2 )
+  {
+    switch(cmd.itype)
+    {
+      case PPC_add:
+        do_add(a);
+       break;
+      case PPC_lis:
+        do_lis();
+       break;
+      case PPC_mtlr:
+       break;
+      case PPC_ori:
+        do_ori();
+       break;
+
+ //     default:
+ //      reset();
+    }
+  }
+  return m_status;
+}
---
+++ MachOPIC/ppc_e.h
@@ -0,0 +1,83 @@
+#ifndef PPC_E_H
+# define PPC_E_H	"Lame PowerPC emulator for PIC addresses resolving"
+
+#include <ida.hpp>
+#include <idp.hpp>
+#include <loader.hpp>
+#include <kernwin.hpp>
+#include <bytes.hpp>
+
+#include "smac.h"
+
+inline bool idaapi
+exists_reg(op_t *op)
+{
+  return (op->type == o_reg);
+}
+
+inline bool idaapi
+exists_phrase(op_t *op)
+{
+  return (op->type == o_phrase || op->type == o_displ );
+}
+
+inline bool idaapi
+exists_value(op_t *op)
+{
+  return (op->type == o_imm ||
+   ((op->type == o_displ) && (op->flags & OF_OUTER_DISP))
+  );
+}
+
+inline bool idaapi
+exists_addr(op_t *op)
+{
+  return (op->type == o_mem || op->type == o_far || op->type == o_near ||
+  ((op->type == o_displ) && !(op->flags & OF_NO_BASE_DISP))
+  );
+}
+
+struct ppc_greg
+{
+  unsigned long value;
+  char is_const;
+};
+
+class ppc_lemul
+{
+  public:
+   ppc_lemul(smac_reffer *);
+   void reset();
+   int emul(ea_t);
+   inline int status()	{ return m_status; }
+   inline ea_t base_addr()
+   {
+     if ( !m_status )
+      return BADADDR;
+     return m_base;
+   }
+   inline int basreg()
+   {
+     if ( !m_status )
+      return 0;
+     return m_basereg;
+   }
+
+   static char *reg_name(int r);
+  protected:
+   smac_reffer *m_reffer;
+
+   char m_cmt[80];
+   struct ppc_greg m_regs[32];
+   int m_status;
+   ea_t m_base;
+   int m_basereg;
+
+   void do_cmt(ea_t, int reg, ea_t value);
+   int is_pic_b(ea_t);
+   int is_mflr();
+   void do_lis();
+   void do_ori();
+   void do_add(ea_t);
+};
+#endif /* PPC_E_H */
