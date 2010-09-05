8f1c67f8b335efc57f429c99345a268b592d1551
redplait <redplait@b0c7a04d-6c7b-4c1e-b982-2d7b310faa71>
2010-09-05 18:25:10 +0000

    add more clean return error codes

--- pdbdump/rp.cpp
+++ pdbdump/rp.cpp
@@ -2,20 +2,22 @@
 #include <ole2.h>
 
 
-long __stdcall NoRegCoCreate(char const *name, REFIID g1, REFIID g2, void **lp)
+HRESULT __stdcall NoRegCoCreate(char const *name, REFIID g1, REFIID g2, void **lp)
 {
   HMODULE m = LoadLibrary(name);
-  long (__stdcall *fn)(REFCLSID, REFIID, LPVOID *);
+  HRESULT (__stdcall *fn)(REFCLSID, REFIID, LPVOID *);
 
   if ( NULL == m )
-   return 1;
-  fn = (long (__stdcall *)(REFCLSID, REFIID, LPVOID *))GetProcAddress(m, "DllGetClassObject");
+   return TYPE_E_CANTLOADLIBRARY;
+  fn = (HRESULT (__stdcall *)(REFCLSID, REFIID, LPVOID *))GetProcAddress(m, "DllGetClassObject");
   if ( NULL == fn )
-   return 1;
-  IClassFactory *cf;
-  long res = fn(g1, IID_IClassFactory, (LPVOID *)&cf);
+   return TYPE_E_BADMODULEKIND;
+  IClassFactory *cf = NULL;
+  HRESULT res = fn(g1, IID_IClassFactory, (LPVOID *)&cf);
+  if ( cf == NULL )
+    return res;
 //  _asm int 3
-  cf->CreateInstance(0,  g2, lp);
+  res = cf->CreateInstance(0,  g2, lp);
   cf->Release();
-  return (*lp == NULL);
+  return res;
 }
