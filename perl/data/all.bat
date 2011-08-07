8406d319d36defaec710a191a57d1efb5b8e7d3f
redplait <redplait@b0c7a04d-6c7b-4c1e-b982-2d7b310faa71>
2011-08-07 11:54:49 +0000

    fix in build bat file

--- perl/data/all.bat
+++ perl/data/all.bat
@@ -3,4 +3,6 @@ perl horror.pl -t i
 perl horror.pl -t c
 perl horror.pl -t pm
 d:\sdk\swigwin-2.0.4\swig.exe -nocontract -perl -static ida.i
-sed 
+sed s/IDAc/IDA/ ida_wrap.c > ..\ida_wrap.c
+copy /y ida.cpp ..
+copy /y ida.h ..
